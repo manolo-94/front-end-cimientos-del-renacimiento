bda0214b03dade843bab855bba0d39fdb1f086d4
Manolo <manolovp94@gmail.com>
2025-07-12 10:59:25 -0600

    Crear Seccion Transparencia

--- src/presentation/features/Transparencia/Transparencia.tsx
+++ src/presentation/features/Transparencia/Transparencia.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import TitleSection from "../../components/TitleSection";
+import "./Transparencia.css"
+import  auditores from "../../../assets/img/auditores.jpg"
+import reportes from "../../../assets/img/reportes.png"
 
 const Fidecomiso: React.FC = () => {
     return (
-        <div>
-            <h1>Fidecomiso</h1>
-            <p>Contenido de la sección de Fidecomiso.</p>
-        </div>
+        <section id="Transparencia">
+            <TitleSection title="Transparencia" />
+            <div className="transparencia">
+                <div>
+                    <img src={reportes} alt="Reporte" className="transparencia-img" />
+                    <p>Accede a reportes trimestrales y documentación oficial.</p>
+                </div>
+                <div>
+                    <img src={auditores} alt="Auditoría" className="transparencia-img" />
+                    <p>Auditorías públicas disponibles para descarga.</p>
+                </div>
+            </div>
+        </section>
     );
 };      
 
